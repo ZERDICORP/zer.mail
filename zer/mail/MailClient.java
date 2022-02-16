ab00e70e7231a4d4516f2cf9941fbe1f099b32b4
ZERDICORP <56264511+ZERDICORP@users.noreply.github.com>
2022-02-16 10:26:38 +0300

    fix tabulation

--- zer/mail/MailClient.java
+++ zer/mail/MailClient.java
@@ -15,13 +15,13 @@ import javax.mail.internet.MimeMessage;
 
 public class MailClient
 {
-	private Properties props;
+  private Properties props;
   private String senderEmail;
   private String senderPassword;
-	private String subject;
-	private String content;
+  private String subject;
+  private String content;
   private String contentType;
-	private String receiverEmail;
+  private String receiverEmail;
 
   {
     props = new Properties();
@@ -32,50 +32,50 @@ public class MailClient
     props.setProperty("mail.smtp.port", "587");
   
     contentType = "text/plain";
-  }	
+  } 
 
-	public MailClient contentType(String contentType)
-	{
-		this.contentType = contentType;
-		return this;
-	}
+  public MailClient contentType(String contentType)
+  {
+    this.contentType = contentType;
+    return this;
+  }
 
-	public MailClient message(String subject, String content)
-	{
-		this.subject = subject; 
-		this.content = content;
-		return this;
-	}
+  public MailClient message(String subject, String content)
+  {
+    this.subject = subject; 
+    this.content = content;
+    return this;
+  }
 
-	public MailClient from(String senderEmail, String senderPassword)
-	{
-		this.senderEmail = senderEmail;
-		this.senderPassword = senderPassword;
-		return this;
-	}
+  public MailClient from(String senderEmail, String senderPassword)
+  {
+    this.senderEmail = senderEmail;
+    this.senderPassword = senderPassword;
+    return this;
+  }
 
-	public MailClient to(String receiverEmail)
-	{
-		this.receiverEmail = receiverEmail;
-		return this;
-	}
+  public MailClient to(String receiverEmail)
+  {
+    this.receiverEmail = receiverEmail;
+    return this;
+  }
 
-	public void send()
-	{
+  public void send()
+  {
     Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() { return new PasswordAuthentication(senderEmail, senderPassword); }
-		});
+      protected PasswordAuthentication getPasswordAuthentication() { return new PasswordAuthentication(senderEmail, senderPassword); }
+    });
 
-		try
-		{
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(senderEmail));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverEmail));
-			message.setSubject(subject);
-			message.setContent(content, contentType);
-			
-			Transport.send(message);
-		}
-		catch (MessagingException e) { throw new RuntimeException(e); }
-	}
+    try
+    {
+      Message message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(senderEmail));
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverEmail));
+      message.setSubject(subject);
+      message.setContent(content, contentType);
+      
+      Transport.send(message);
+    }
+    catch (MessagingException e) { throw new RuntimeException(e); }
+  }
 }
